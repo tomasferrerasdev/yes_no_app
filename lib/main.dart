0b06b8434fff076da4811c6424b79220ef63a590
Tomas Ferreras <hellotomasdev@gmail.com>
2023-05-28 13:02:52 -0300

    feat: add global theme with custom colors

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:yes_no_app/config/theme/app_theme.dart';
 
 void main() => runApp(const MyApp());
 
@@ -9,6 +10,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Yes No App',
+      theme: AppTheme().theme(),
       debugShowCheckedModeBanner: false,
       home: Scaffold(
         appBar: AppBar(
