17b7ffecdc1270dca3943cd42108a29a2cbab67a
Tomas Ferreras <hellotomasdev@gmail.com>
2023-05-28 15:38:16 -0300

    feat: add basic textInput behavior

--- lib/presentation/widgets/shared/message_field_box.dart
+++ lib/presentation/widgets/shared/message_field_box.dart
@@ -5,28 +5,36 @@ class MessageFieldBox extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final textController = TextEditingController();
+    final FocusNode focusNode = FocusNode();
+
     final outlineInputBorder = UnderlineInputBorder(
         borderSide: const BorderSide(color: Colors.transparent),
         borderRadius: BorderRadius.circular(40));
 
     final inputDecoration = InputDecoration(
+        hintText: 'End your message with a "??"',
         filled: true,
         focusedBorder: outlineInputBorder,
         enabledBorder: outlineInputBorder,
         suffixIcon: IconButton(
           icon: const Icon(Icons.send),
           onPressed: () {
-            print('Text box value');
+            final textValue = textController.value.text;
+            print('button: $textValue');
+            textController.clear();
           },
         ));
 
     return TextFormField(
+      onTapOutside: (event) => focusNode.unfocus(),
+      focusNode: focusNode,
       decoration: inputDecoration,
+      controller: textController,
       onFieldSubmitted: (value) {
         print('Submit value $value');
-      },
-      onChanged: (value) {
-        print('change $value');
+        textController.clear();
+        focusNode.requestFocus();
       },
     );
   }
